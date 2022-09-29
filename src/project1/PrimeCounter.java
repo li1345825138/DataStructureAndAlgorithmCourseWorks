7dadd83ce58d1e7908bb428145f62c7ab678c2e5
li1345825138 <rk1234559@gmail.com>
2022-09-28 20:04:59 -0400

    add PrimeCounter class

--- src/project1/PrimeCounter.java
+++ src/project1/PrimeCounter.java
@@ -45,7 +45,7 @@ public class PrimeCounter {
             throw new IllegalArgumentException("Error: number couldn't less than 0!");
         }
         int count = 0;
-        for (int i = 0; i <= n; i++) {
+        for (int i = 2; i <= n; i++) {
             if (isPrime(i)) {
                 count++;
             }
